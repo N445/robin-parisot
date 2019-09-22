80ffb2ae7d2e6111b66cc96f7d5bb04d0eddb658
Robin <parisrob@hotmail.fr>
2019-09-22 19:54:01 +0200

    add contact

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Utils\ToolsProvider;
 
@@ -10,13 +13,22 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $contact = new Contact();
+        $form    = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // send contact
+        }
         return $this->render('default/index.html.twig', [
             'toolsFront' => ToolsProvider::getToolsFront(),
             'toolsBack'  => ToolsProvider::getToolsBack(),
             'toolsOther' => ToolsProvider::getToolsOther(),
+            'form'       => $form->createView(),
         ]);
     }
 }
