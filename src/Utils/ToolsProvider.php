07629d1e3c8cdad3fab258777d2339f909aefb94
Robin <parisrob@hotmail.fr>
2019-09-22 19:37:36 +0200

    WIP integration

--- src/Utils/ToolsProvider.php
+++ src/Utils/ToolsProvider.php
@@ -4,7 +4,11 @@ namespace App\Utils;
 
 class ToolsProvider
 {
-    const NAME = 'name';
+    const NAME      = 'name';
+    const LINK      = 'link';
+    const IMAGE     = 'image';
+    const VERSIONS   = 'versions';
+    const BASE_PATH = 'images/tools/';
 
     /**
      * @return array
@@ -13,19 +17,34 @@ class ToolsProvider
     {
         return [
             [
-                self::NAME => 'Bootsrap',
+                self::NAME    => 'Bootsrap',
+                self::LINK    => '//getbootstrap.com',
+                self::VERSIONS => ['3', '4'],
+                self::IMAGE   => self::BASE_PATH . 'bootstrap.png',
             ],
             [
-                self::NAME => 'Grunt',
+                self::NAME  => 'Grunt',
+                self::LINK  => '//gruntjs.com',
+                self::IMAGE => self::BASE_PATH . 'grunt.png',
             ],
             [
-                self::NAME => 'Webpack',
+                self::NAME  => 'Webpack',
+                self::LINK  => '//webpack.js.org',
+                self::IMAGE => self::BASE_PATH . 'webpack.png',
             ],
             [
-                self::NAME => 'SASS',
+                self::NAME  => 'SASS',
+                self::LINK  => '//sass-lang.com',
+                self::IMAGE => self::BASE_PATH . 'sass.png',
             ],
             [
-                self::NAME => 'Javascript',
+                self::NAME  => 'Javascript',
+                self::IMAGE => self::BASE_PATH . 'javascript.png',
+            ],
+            [
+                self::NAME  => 'jQuery',
+                self::LINK  => '//jquery.com',
+                self::IMAGE => self::BASE_PATH . 'jquery.png',
             ],
         ];
     }
@@ -37,7 +56,9 @@ class ToolsProvider
     {
         return [
             [
-                self::NAME => 'Symfony',
+                self::NAME  => 'Symfony',
+                self::LINK  => '//symfony.com',
+                self::IMAGE => self::BASE_PATH . 'symfony.png',
             ],
         ];
     }
@@ -49,7 +70,9 @@ class ToolsProvider
     {
         return [
             [
-                self::NAME => 'GIT',
+                self::NAME  => 'GIT',
+                self::LINK  => '//git-scm.com',
+                self::IMAGE => self::BASE_PATH . 'git.png',
             ],
         ];
     }
