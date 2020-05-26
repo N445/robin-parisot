39e1e261c4d55f4ec26a1c3e230ed909e1f5cff2
Robin PARISOT <parisrob@hotmail.fr>
2020-05-26 12:10:49 +0000

    add truc fun

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    console.log('%cBonjour à toi le DEV ! :D', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
     // $('[data-toggle="popover"]').popover();
     // highlight.initHighlightingOnLoad();
     hljs.initHighlightingOnLoad();
@@ -36,13 +36,11 @@ $(document).ready(function () {
 
     $('body').on('click', '.contact-send', function (e) {
         e.preventDefault();
-        console.log($(this).parent('form').serialize());
         $.ajax({
             url: Routing.generate('homepage'),
             method: "POST",
             data: $(this).parent('form').serialize()
         }).always(function (data) {
-            console.log(data);
             if (data.success) {
                 new Noty({
                     type: 'success',
